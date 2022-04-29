47960eb6354befc7c3ab4e7c1312940d6dae30a9
ajcastro <ajcastro29@gmail.com>
2022-04-29 10:28:06 +0800

    Fix

--- src/views/admin/user_logs/List.vue
+++ src/views/admin/user_logs/List.vue
@@ -250,6 +250,8 @@ export default {
       resourceId: null,
       model: UserLog,
       ...makeTable({
+        sortBy: 'date',
+        sortDesc: true,
         filter: {
           search: '',
           date_range: '',
